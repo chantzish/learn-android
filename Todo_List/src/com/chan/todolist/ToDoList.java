1985c188d86ebd003e7ad0b3f3ed6973f9ea3fbe
chantzish1 <chantzish1@gmail.com>
2019-06-28 16:40:36 +0000

    change onClick

--- Todo_List/src/com/chan/todolist/ToDoList.java
+++ Todo_List/src/com/chan/todolist/ToDoList.java
@@ -7,26 +7,25 @@ import android.view.View;
 import android.widget.*;
 
 public class ToDoList extends Activity {
+	EditText myEditText;
+	ArrayList<String> toDoItems;
+	ArrayAdapter<String> aa;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         ListView myListView = (ListView)findViewById(R.id.myListView);
-        final EditText myEditText = (EditText)findViewById(R.id.myEditText);
-        final Button myButton = (Button)findViewById(R.id.myButton);
-        final ArrayList<String> toDoItems = new ArrayList<String>();
-        final ArrayAdapter<String> aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,toDoItems);
+        myEditText = (EditText)findViewById(R.id.myEditText);
+        //Button myButton = (Button)findViewById(R.id.myButton);
+        toDoItems = new ArrayList<String>();
+        aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,toDoItems);
         myListView.setAdapter(aa);
-        myButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				toDoItems.add(0, myEditText.getText().toString());
-				aa.notifyDataSetChanged();
-				myEditText.setText("");
-			}
-		});
     }
+	public void onClick(View v) {
+		toDoItems.add(0, myEditText.getText().toString());
+		aa.notifyDataSetChanged();
+		myEditText.setText("");
+	}
 }
